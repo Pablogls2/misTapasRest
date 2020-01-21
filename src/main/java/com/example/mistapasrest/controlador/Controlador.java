9ee59075b18abc67cf25ad2eae495275d0ee1cf8
pablogls2 <pablopradoruiz@gmail.com>
2020-01-21 10:20:33 +0100

    controlador y usuario

---
+++ src/main/java/com/example/mistapasrest/controlador/Controlador.java
@@ -0,0 +1,17 @@
+package com.example.mistapasrest.controlador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controlador {
+
+
+
+    @GetMapping
+    @RequestMapping(value="hola",method= RequestMethod.GET)
+    public String hola(){return "hola chicos, vuestro lider avanza";}
+}
---
+++ src/main/java/com/example/mistapasrest/modelos/Usuario.java
@@ -0,0 +1,38 @@
+package com.example.mistapasrest.modelos;
+
+public class Usuario {
+
+    private int id;
+    private String email;
+    private String psw;
+
+    public Usuario(int id, String email, String psw) {
+        this.id = id;
+        this.email = email;
+        this.psw = psw;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+}
