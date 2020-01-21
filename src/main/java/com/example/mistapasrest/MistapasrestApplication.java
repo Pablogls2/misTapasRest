6d9ec500dc8cddbbe29b5d3719bd300e0bd78e36
pablogls2 <pablopradoruiz@gmail.com>
2020-01-21 09:42:59 +0100

    recien creado

---
+++ src/main/java/com/example/mistapasrest/MistapasrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mistapasrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistapasrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MistapasrestApplication.class, args);
+    }
+
+}
