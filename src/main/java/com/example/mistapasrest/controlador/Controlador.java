0985cc021d3bc2ec0b5213d2efcabd986766828c
pablogls2 <pablopradoruiz@gmail.com>
2020-01-25 20:11:10 +0100

    Funcional

---
+++ src/main/java/com/example/mistapasrest/controlador/Controlador.java
@@ -0,0 +1,28 @@
+package com.example.mistapasrest.controlador;
+
+import com.example.mistapasrest.modelos.Usuario;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controlador {
+
+
+
+    @GetMapping
+    @RequestMapping(value="hola",method= RequestMethod.GET)
+    public String hola(){return "hola chicos, vuestro lider avanza";}
+
+
+
+    @RequestMapping(value="usuarios",method= RequestMethod.GET)
+    public ResponseEntity<Usuario> getUsuario(){
+       Usuario u= new Usuario ();
+
+       return ResponseEntity.ok(u);
+    }
+}
