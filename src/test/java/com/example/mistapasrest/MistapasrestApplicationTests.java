0dfdc0a6d9db754dfb445857d130597516c54170
pablogls2 <pablopradoruiz@gmail.com>
2020-01-27 23:33:01 +0100

    avanza

---
+++ src/test/java/com/example/mistapasrest/MistapasrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mistapasrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistapasrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
